af29cab42a759dbc53afb95738edfd35f82154f7
Rachele Edwards <dot.com1472@gmail.com>
2020-04-27 15:46:23 -0600

    added forms to EditForm and AppPlantForm--api is now accessible so I can go back to my requests-woot

---
+++ water-my-plants/src/components/EditForm.js
@@ -0,0 +1,66 @@
+import React, {useState, useEffect} from "react"
+import {useParams, useHistory} from "react-router-dom"
+import axios from "axios"
+
+const initialPlant = {
+    id: "",
+    nickname: "",
+    species: "",
+    h2oFrequency: "",
+    imageURL: ""
+}
+//useEffect for initial load for plant data--GET
+
+//changeHandler
+
+//saveItem onSubmit-->PUT
+
+
+const EditForm = () => {
+    const [plant, setPlant] = useState(initialPlant)
+    // const {push} = useHistory()
+
+    return(
+        <>
+        <h2>Edit Plant</h2>
+            <p>Fill out the updated information</p>
+            <div className="login-form">
+                <form /*add onSubmit*/> 
+                    <label htmlFor="nickname">Nickname: &nbsp;
+                        <input 
+                        id="nickname"
+                        name="nickname"
+                        value={plant.nickname}
+                        onChange="" //add onChange event here
+                        /></label> &nbsp;
+                     <label htmlFor="species">Species: (optional) &nbsp;
+                        <input 
+                        id="species"
+                        name="species"
+                        value={plant.species}
+                        onChange="" //add onChange event here
+                        /></label> &nbsp;
+                    <label htmlFor="h2oFrequency">
+                        <select id="h2oFrequency" name="h2oFrequency">
+                            <option value="low" >Once a month</option>
+                            <option value="medium" >Once a week</option>
+                            <option value="medium-high" >Once a day</option>
+                            <option value="high" >Twice a day</option>
+                        </select>
+                        Water Frequency: &nbsp;</label>
+                    <label htmlFor="imageURL">
+                        <input 
+                        type="string"
+                        id="imageURL"
+                        name="imageURL"
+                        value={plant.imageURL}
+                        onChange="" //add onChange event here
+                        /></label> &nbsp;
+                    <button>Save</button>
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default EditForm
