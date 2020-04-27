73f2c341495116811aec714ae4578e14adeac9cf
Rachele Edwards <dot.com1472@gmail.com>
2020-04-27 12:03:37 -0600

    imported Login into App

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom"
 import PrivateRoute from './components/PrivateRoute';
 import HomePage from "./components/HomePage"
+import Login from "./components/Login"
 
 function App() {
   return (
     <Router>
       <div className="App">
         {/* put components in here */}
-        <Route exact path ="/" component={Login} />
+        <Route exact path ="/login" component={Login} />
         <PrivateRoute exact path="/homepage" component={HomePage} />
       </div>
     </Router>
