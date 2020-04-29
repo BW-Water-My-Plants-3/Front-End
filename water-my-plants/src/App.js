bf804b970108d92a5fe9e7e938a80a11759329ca
strengthofthemountain <strengthofthemountain@gmail.com>
2020-04-29 11:41:54 -0400

    initial commit

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -3,12 +3,14 @@ import {BrowserRouter as Router, Route} from "react-router-dom"
 import PrivateRoute from './components/PrivateRoute';
 import HomePage from "./components/HomePage"
 import Login from "./components/Login"
+import Signup from './components/Signup';
 function App() {
   return (
     <Router>
       <div className="App">
         {/* put components in here */}
         <Route exact path ="/login" component={Login} />
+        <Route exact path ="/signup" component={Signup} />
         <PrivateRoute exact path="/homepage" component={HomePage} />
       </div>
     </Router>
